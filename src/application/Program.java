a1b18d862f631e25063f57d2a18e8263a40d56b1
Lascarv <lais.desantana05@gmail.com>
2026-07-07 13:12:18 -0300

    Implement findById method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -9,16 +7,15 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main (String[] args) {
-		
-		Department dep = new Department(2, "Eletronics");
-		
+	public static void main (String[] args) {	
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();		
 		
 		// testando insert
-		Seller newSeller = new Seller(null, "anapaula", "ana@gmail.com", new Date(), 600.0, dep);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserido! Novo id = " + newSeller.getId());
+		Seller seller = sellerDao.findById(2);
+		
+		System.out.println(seller);
+		
+		
 	}
 }
