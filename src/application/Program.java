a87c6d3fa67bdd08fdaeb8b48b357fddfddaa1aa
Lascarv <lais.desantana05@gmail.com>
2026-07-10 16:45:13 -0300

    Implement findAll method

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,23 +9,31 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main (String[] args) {	
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();		
-		
+	public static void main(String[] args) {
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		// teste findById
 		System.out.println("Teste: findByID");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		System.out.println();
-		
-		//teste FindByDepartment
+
+		// teste FindByDepartment
 		System.out.println("Teste: findByDepartment");
 		Department dep = new Department(2, null);
-		List <Seller> list = sellerDao.findByDepartment(dep);
-		
+		List<Seller> list = sellerDao.findByDepartment(dep);
 		for (Seller obj : list) {
-		System.out.println(obj);
+			System.out.println(obj);
+		}
+		
+		System.out.println();
+
+		// teste findAll
+		System.out.println("Teste: findAll");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
 		}
 	}
 }
