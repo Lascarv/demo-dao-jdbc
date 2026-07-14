8027e222d089da65d27e489635db43c8a25990bf
Lascarv <lais.desantana05@gmail.com>
2026-07-14 20:28:40 -0300

    Implement update method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -21,7 +22,7 @@ public class Program {
 
 		// teste FindByDepartment
 		System.out.println("Teste: findByDepartment");
-		Department dep = new Department(2, null);
+		Department dep = new Department(3, null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
 		for (Seller obj : list) {
 			System.out.println(obj);
@@ -35,5 +36,23 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+		
+		// teste insert
+		System.out.println("Teste: insert");
+		Seller newSeller = new Seller(null, "Amanda", "amanda@gmail.com", new Date(), 5000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserido: novo Id = " + newSeller.getId());
+		
+		System.out.println();
+		
+		// teste update
+		System.out.println("Teste: update");
+		seller = sellerDao.findById(3);
+		seller.setName("Gabriel");
+		seller.setEmail("gabriel@gmail.com");
+		sellerDao.update(seller);
+		
 	}
 }
